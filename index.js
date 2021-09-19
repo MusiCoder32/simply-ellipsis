390a9584e551f99d74a155dc2651a2d388f903f1
wyj <w327134@163.com>
2021-09-20 03:06:23 +0800

    将所有伪类的top/left置为0,保证其全部出现在可视区域，解决不在视野时出现的高宽计算异常情况

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import setEll from "./src/setEll.js";
 import setObserver from "./src/setObserver.js";
 
 export default function startEllipsis(id) {
-  setEll()
   window.onresize = function () {
     setEll()
   }
   window.onload = function () {
-    setObserver(id)
+      setEll()
+      setObserver(id)
   }
 }
